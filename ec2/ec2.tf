eadf0bfa9d5629a6622aa4ee1d9461e05cc0b61a
balaguru-20 <balaguravaiah0606@gmail.com>
2025-04-23 01:14:41 +0530

    terraform

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "this" {
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   instance_type          = "t3.micro"
   tags = {
-    Name    = "terraform_demo-change"
+    Name    = "terraform_demo"
     purpose = "terrsform_practice"
   }
 
